d4e5d6adfc47297456aec6662ae476cecbaf8695
Nobby Knox <nobbyknox@gmail.com>
2015-12-06 13:27:53 +0200

    Inform user to sign in before bookmarking pages

--- index.js
+++ index.js
@@ -1,66 +1,89 @@
-var { ToggleButton } = require('sdk/ui/button/toggle');
+var {
+    ToggleButton
+} = require('sdk/ui/button/toggle');
 var panels = require("sdk/panel");
 var tabs = require("sdk/tabs");
 var self = require("sdk/self");
 
-var {Cc, Ci} = require("chrome");
-// var cookieSvc = Cc["@mozilla.org/cookieService;1"].getService(Ci.nsICookieService);
+var {
+    Cc, Ci
+} = require("chrome");
 
 var cookieManager = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager2);
 
 var data = require("sdk/self").data;
 var contentScriptFile = data.url("get-action.js");
 
+var baseUrl = 'http://127.0.0.1:3003'
+
 var button = ToggleButton({
-  id: "my-button",
-  label: "my button",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
-  onChange: handleChange
+    id: "my-button",
+    label: "my button",
+    icon: {
+        "16": "./icon-16.png",
+        "32": "./icon-32.png",
+        "64": "./icon-64.png"
+    },
+    onChange: handleChange
 });
 
 var panel = panels.Panel({
-  contentURL: self.data.url("panel.html"),
-  onHide: handleHide,
-  contentScriptFile: contentScriptFile
+    contentURL: self.data.url("panel.html"),
+    onHide: handleHide,
+    contentScriptFile: contentScriptFile,
+    width: 270,
+    height: 100
+});
+
+var noTokenPanel = panels.Panel({
+    contentURL: self.data.url("no-token.html"),
+    width: 300,
+    height: 200
 });
 
 function handleChange(state) {
-  if (state.checked) {
-    panel.show({
-      position: button
-    });
-  }
+    if (state.checked) {
+        panel.show({
+            position: button
+        });
+    }
 }
 
 function handleHide() {
-  button.state('window', {checked: false});
+    button.state('window', {
+        checked: false
+    });
 }
 
-panel.port.on("goingHome", function() {
-  console.log('In index.js - going home');
-  panel.hide();
+panel.port.on("goingHome", function () {
+    tabs.open(baseUrl + '/');
+    panel.hide();
 });
 
-panel.port.on("bookmarkPage", function() {
+panel.port.on("bookmarkPage", function () {
 
-  var cookies = cookieManager.getCookiesFromHost("leonieknox.com");
-  // var cookies = cookieManager.enumerator
+    var cookies = cookieManager.getCookiesFromHost("leonieknox.com");
+    // var cookies = cookieManager.enumerator
+    var foundCookie = false;
 
-  while (cookies.hasMoreElements()) {
-    var cookie = cookies.getNext().QueryInterface(Ci.nsICookie2);
+    while (cookies.hasMoreElements()) {
+        var cookie = cookies.getNext().QueryInterface(Ci.nsICookie2);
 
-    if (cookie.name === 'bookmarklyLogin') {
-        dump(cookie.host + ";" + cookie.name + "=" + cookie.value + "\n");
-        var cookieValue = JSON.parse(unescape(cookie.value));
-        // console.log(JSON.stringify(cookieValue));
-        // console.log('Screen name: ' + cookieValue['screenName']);
-        console.log('Token: ' + cookieValue.token);
+        if (cookie.name === 'bookmarklyLogin') {
+            dump(cookie.host + ";" + cookie.name + "=" + cookie.value + "\n");
+            var cookieValue = JSON.parse(unescape(cookie.value));
+            // console.log(JSON.stringify(cookieValue));
+            // console.log('Screen name: ' + cookieValue['screenName']);
+            console.log('Token: ' + cookieValue.token);
+            foundCookie = true;
+        }
     }
-  }
 
-  panel.hide();
+    if (!foundCookie) {
+        noTokenPanel.show({
+            position: button
+        });
+    }
+
+    panel.hide();
 });
